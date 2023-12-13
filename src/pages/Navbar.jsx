eb4ed38d6615f16db637ff80f3c7b942bfd678eb
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2023-12-13 19:50:49 +0600

    added my skills sections

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import { AiOutlineBars } from 'react-icons/ai'
 import img from "../assets/WhatsApp Image 2023-12-12 at 13.09.37_81b78cc6.jpg"
 import { MdOutlineClose } from "react-icons/md";
-import { FaTwitter } from "react-icons/fa";
+import { FaDownload, FaTwitter } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareFacebook } from "react-icons/fa6";
 import { FaSquareGithub } from "react-icons/fa6";
@@ -19,6 +19,17 @@ import { FaPencilRuler } from "react-icons/fa";
 import { PiPaletteFill } from "react-icons/pi";
 import { FaDesktop } from "react-icons/fa";
 import { FaPaintBrush } from "react-icons/fa";
+import html from "../assets/html.png"
+import css from "../assets/css.png"
+import tailwind from "../assets/nwt9ncojkvwmjfkaada8upafvpnu-removebg-preview.png"
+import js from "../assets/JavaScript-logo-removebg-preview.png"
+import react from "../assets/react.png"
+import Mongodb from "../assets/download-removebg-preview.png"
+import Express from "../assets/express.png"
+import node from "../assets/node-removebg-preview (1).png"
+import github from "../assets/png-transparent-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper-banner-thumbnail-removebg-preview.png"
+
+
 
 
 const Navbar = () => {
@@ -299,13 +310,94 @@ const Navbar = () => {
                         </div>
                     </div>
                 </section>
-                <section>
+                <section className=' max-w-5xl mx-auto'>
                     <div className='my-12'>
                         <h1 className='text-3xl text-center font-bold'>My Skills</h1>
                         <hr className=' bg-green-400 h-1 w-16 mt-2 mx-auto' />
+                        <div className='my-10'>
+
+                            <div className='grid grid-cols-2 md:grid-cols-3  gap-2'>
+
+                                <div className='flex flex-col items-center justify-center rounded-xl shadow-xl space-y-2  h-64 bg-gray-400'>
+                                    <img src={css} alt="" className='w-40 object-cover' />
+
+                                    <progress className="progress progress-primary w-40" value="95" max="100"></progress>
+                                    <p className='text-white'>HTML5</p>
+                                </div>
+                                <div className='flex flex-col items-center justify-center rounded-xl shadow-xl space-y-2 
+                                 h-64 bg-gray-400'>
+                                    <img src={html} alt="" className='w-40 object-cover' />
+
+                                    <progress className="progress progress-primary w-40" value="90" max="100"></progress>
+                                    <p className='text-white'>CSS3</p>
+                                </div>
+                                <div className='flex flex-col items-center justify-center rounded-xl shadow-xl space-y-2 
+                                 h-64 bg-gray-400'>
+                                    <img src={tailwind} alt="" className='w-40 object-cover' />
+
+                                    <progress className="progress progress-primary w-32" value="90" max="100"></progress>
+                                    <p className='text-white'>Tailwind</p>
+                                </div>
+                                <div className='flex flex-col items-center justify-center rounded-xl shadow-xl space-y-2 
+                                 h-64 bg-gray-400'>
+                                    <img src={js} alt="" className='w-40 object-cover' />
+
+                                    <progress className="progress progress-primary w-40" value="90" max="100"></progress>
+                                    <p className='text-white'>JavaScript</p>
+                                </div>
+
+
+                                <div className='flex flex-col items-center justify-center rounded-xl shadow-xl space-y-2 
+                                 h-64 bg-gray-400'>
+                                    <img src={react} alt="" className='w-40 object-cover' />
+
+                                    <progress className="progress progress-primary w-40" value="85" max="100"></progress>
+                                    <p className='text-white'>React js</p>
+                                </div>
+                                <div className='flex flex-col items-center justify-center rounded-xl shadow-xl space-y-2 
+                                 h-64 bg-gray-400'>
+                                    <img src={node} alt="" className='w-40 object-cover' />
+
+                                    <progress className="progress progress-primary w-40" value="40" max="100"></progress>
+                                    <p className='text-white'>Node js</p>
+                                </div>
+                                <div className='flex flex-col items-center justify-center rounded-xl shadow-xl space-y-2 
+                                 h-64 bg-gray-400'>
+                                    <img src={Express} alt="" className='w-40  object-cover' />
+
+                                    <progress className="progress progress-primary w-40" value="50" max="100"></progress>
+                                    <p className='text-white'>Express js</p>
+
+                                </div>
+                                <div className='flex flex-col items-center justify-center rounded-xl shadow-xl space-y-2 
+                                 h-64 bg-gray-400'>
+                                    <img src={Mongodb} alt="" className='w-40  object-cover' />
+
+                                    <progress className="progress progress-primary w-40" value="60" max="100"></progress>
+                                    <p className='text-white'>Mongodb</p>
+
+                                </div>
+                                <div className='flex flex-col items-center justify-center rounded-xl shadow-xl space-y-2 
+                                 h-64 bg-gray-400'>
+                                    <img src={github} alt="" className='w-40  object-cover' />
+
+                                    <progress className="progress progress-primary w-40" value="70" max="100"></progress>
+                                    <p className='text-white'>Github</p>
+
+                                </div>
+                                <a href="/public/junayet-shiblu.pdf" download="junayet resume.pdf">
+                                    <button className=' btn btn-outline text-xl btn-accent'>Download CV <FaDownload></FaDownload></button>
+                                </a>
+
+
+                            </div>
+
+
+                        </div>
 
                     </div>
                 </section>
+
             </div>
         </div >
     );
