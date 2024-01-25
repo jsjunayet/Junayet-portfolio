3e5e114b9e3777bf6418f08b4d96ed8e011d4bf1
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2024-01-26 01:12:25 +0600

    added update my site

--- src/pages/Porfolio.jsx
+++ src/pages/Porfolio.jsx
@@ -1,48 +1,92 @@
-
+// Import statements
+import React from 'react';
 import { Link } from 'react-router-dom';
 import ShareTitle from '../Component/ShareTitle';
-import img from "../assets/Screenshot 2023-12-13 224916.png";
-import img1 from "../assets/Screenshot 2023-12-13 225008.png";
-import img2 from "../assets/Screenshot 2023-12-13 225129.png";
-import img3 from "../assets/Screenshot 2023-12-13 225222.png";
-import img4 from "../assets/Screenshot 2023-12-13 225252.png";
-import img5 from "../assets/Screenshot 2023-12-13 225323.png";
-import img6 from "../assets/Screenshot 2023-12-13 225359.png";
-import img7 from "../assets/Screenshot 2023-12-13 232440.png";
+import img from '../assets/Screenshot 2023-12-13 224916.png';
+import img1 from '../assets/Screenshot 2023-12-13 225008.png';
+import img2 from '../assets/Screenshot 2023-12-13 225129.png';
+import img3 from '../assets/Screenshot 2023-12-13 225222.png';
+import img4 from '../assets/Screenshot 2023-12-13 225252.png';
+import img5 from '../assets/Screenshot 2023-12-13 225323.png';
+import img6 from '../assets/Screenshot 2023-12-13 225359.png';
+import img7 from '../assets/Screenshot 2023-12-13 232440.png';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+const portfolioItems = [
+    {
+        image: img,
+        link: 'https://splendid-stardust-cf0114.netlify.app/',
+        alt: 'Project 1',
+    },
+    {
+        image: img1,
+        link: 'https://adorable-horse-3c6729.netlify.app/',
+        alt: 'Project 2',
+    },
+    {
+        image: img2,
+        link: 'https://inspiring-churros-226b0a.netlify.app/',
+        alt: 'Project 3',
+    },
+    {
+        image: img3,
+        link: 'https://inspiring-churros-226b0a.netlify.app/',
+        alt: 'Project 4',
+    },
+    {
+        image: img4,
+        link: 'https://incomparable-paprenjak-92812e.netlify.app/',
+        alt: 'Project 5',
+    },
+    {
+        image: img5,
+        link: 'https://incomparable-paprenjak-92812e.netlify.app/',
+        alt: 'Project 6',
+    },
+    {
+        image: img6,
+        link: 'https://beautiful-griffin-33a882.netlify.app/',
+        alt: 'Project 7',
+    },
+    {
+        image: img7,
+        link: 'https://beautiful-griffin-33a882.netlify.app/',
+        alt: 'Project 8',
+    },
+];
 
-
-
-const Porfolio = () => {
+const Portfolio = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+        });
+    }, []);
     return (
         <section id='portfolio'>
             <div>
-                <ShareTitle opactityTitle={"PORTFOLIO"} Title={"My Work"}></ShareTitle>
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-2 mt-10'>
-                    <Link target='"_blank' to={"https://splendid-stardust-cf0114.netlify.app/"}>
-                        <img src={img} className=' md:h-[325px] h-full rounded shadow  object-cover' alt="" />
-                    </Link>
-                    <Link target={"_blank"} to={'https://adorable-horse-3c6729.netlify.app/'}>
-                        <img src={img1} className=' object-cover rounded shadow ' alt="" />
-                    </Link>
-                    <Link target={"_blank"} to={"https://inspiring-churros-226b0a.netlify.app/"}>
-                        <img src={img2} className=' object-cover rounded shadow ' alt="" />
-                    </Link>
-                    <Link to={"https://inspiring-churros-226b0a.netlify.app/"} target={"_blank"}> <img src={img3} className='object-cover rounded shadow ' alt="" /></Link>
-                    <Link target={"_blank"} to={"https://incomparable-paprenjak-92812e.netlify.app/"}>
-                        <img src={img4} className=' object-cover rounded shadow ' alt="" /></Link>
-                    <Link target={"_blank"} to={"https://incomparable-paprenjak-92812e.netlify.app/"}>
-                        <img src={img5} className=' md:h-[325px] h-full  object-cover rounded shadow ' alt="" />
-                    </Link>
-                    <Link target={"_blank"} to={"https://beautiful-griffin-33a882.netlify.app/"}>
-                        <img src={img6} className=' md:h-[325px] h-full  object-cover rounded shadow ' alt="" />
-                    </Link>
-                    <Link target={"_blank"} to={"https://beautiful-griffin-33a882.netlify.app/"}>
-                        <img src={img7} className=' md:h-[325px] h-full object-cover rounded shadow ' alt="" />
-                    </Link>
+                <ShareTitle opacityTitle={'PORTFOLIO'} Title={'My Work'} />
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-10'>
+                    {portfolioItems.map((item, index) => (
+                        <Link
+                            key={index}
+                            to={item.link}
+                            target='_blank'
+                            className='block relative rounded overflow-hidden'
+                            data-aos='fade-up'
+                            data-aos-delay={`${index * 100}`}
+                        >
+                            <img
+                                src={item.image}
+                                className='w-full h-full object-cover transition-transform duration-300 transform hover:scale-105'
+                                alt={item.alt}
+                            />
+                        </Link>
+                    ))}
                 </div>
             </div>
         </section>
     );
 };
 
-export default Porfolio;
+export default Portfolio;
